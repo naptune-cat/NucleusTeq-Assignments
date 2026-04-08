c13040eb367f75eddadbb1b5ce669f20b5749ad5
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-08 09:53:29 +0530

    feat: add array element avg file

---
+++ Java/session1/arrays/FindElementAverage.java
@@ -0,0 +1,19 @@
+package session1.arrays;
+
+import java.util.Scanner;
+public class FindElementAverage {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[] arr = new int[10];
+        double average = 0;
+        int total = 0;
+        System.out.println("Enter array elements (upto 10)");
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = s.nextInt();
+            total += arr[i];
+        }
+        average = total / 10;
+        System.out.println("Average of all elements is "+ average);
+    }
+}
