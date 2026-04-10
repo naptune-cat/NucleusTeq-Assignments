ad24695601230370be6e12a932e1fed9080e7bea
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-10 18:57:53 +0530

    Added and implemented user controller

---
+++ Java/session2/user-management-system/user-management-system/src/main/java/com/example/user_management_system/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.example.user_management_system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user_management_system.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import java.util.List;
+
+import com.example.user_management_system.model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    //for retrieving all the data in our db
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    
+    //for retrieving data of only specific id
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    // for adding a user
+    @PostMapping 
+    public String createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    
+}
