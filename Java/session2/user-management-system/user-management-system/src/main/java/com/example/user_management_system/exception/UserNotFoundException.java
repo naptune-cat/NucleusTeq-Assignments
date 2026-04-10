b49087a92808332779e227cb8d8eda5b5f8946a8
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-10 19:05:11 +0530

    Added custom user not found exception

--- Java/session2/user-management-system/user-management-system/src/main/java/com/example/user_management_system/exception/UserNotFoundException.java
+++ Java/session2/user-management-system/user-management-system/src/main/java/com/example/user_management_system/exception/UserNotFoundException.java
@@ -1,6 +1,10 @@
 package com.example.user_management_system.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 //making custom exception
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message){
         super(message);
