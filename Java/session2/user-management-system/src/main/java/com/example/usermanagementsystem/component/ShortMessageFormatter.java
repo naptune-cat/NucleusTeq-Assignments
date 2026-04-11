c51af92c8c132405622af3eada584a214fd4ff8b
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-11 14:36:06 +0530

    Added Short message component

--- Java/session2/user-management-system/src/main/java/com/example/usermanagementsystem/component/ShortMessageFormatter.java
+++ Java/session2/user-management-system/src/main/java/com/example/usermanagementsystem/component/ShortMessageFormatter.java
@@ -1,5 +1,10 @@
 package com.example.usermanagementsystem.component;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ShortMessageFormatter {
-    
+    public String formatMessage() {
+        return "Sale starts today";
+    }
 }
