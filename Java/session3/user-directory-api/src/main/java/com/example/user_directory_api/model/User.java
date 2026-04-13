d2487a7426902b93b1f9d8a43ddcab07bc6eea61
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-13 21:20:46 +0530

    Implemented user model

--- Java/session3/user-directory-api/src/main/java/com/example/user_directory_api/model/User.java
+++ Java/session3/user-directory-api/src/main/java/com/example/user_directory_api/model/User.java
@@ -1,9 +1,45 @@
-// package com.example.user_directory_api.model;
+package com.example.user_directory_api.model;
 
-// public class User {
-//     private Integer id;
-//     private String name;
-//     private Integer age;
-//     private String role;
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String role;
 
-// }
+    //constructor
+    public User(Integer id, String name, Integer age, String role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    //setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
