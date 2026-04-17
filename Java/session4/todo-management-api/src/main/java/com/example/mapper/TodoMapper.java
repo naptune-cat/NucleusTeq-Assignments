b761efce590afc1cceaa8c5c60041c831774d2d2
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-17 19:07:38 +0530

    refactor: add TodoMapper for DTO conversion and code reusablity

---
+++ Java/session4/todo-management-api/src/main/java/com/example/mapper/TodoMapper.java
@@ -0,0 +1,34 @@
+package com.example.mapper;
+
+import com.example.dto.RequestDTO;
+import com.example.dto.ResponseDTO;
+import com.example.entity.Todo;
+
+public class TodoMapper {
+
+    // for DTO -> Entity mapping
+    
+    public static Todo dtoToEntityMapping(RequestDTO requestDTO) {
+        Todo todo = new Todo();
+
+        todo.setTitle(requestDTO.getTitle());
+        todo.setDescription(requestDTO.getDescription());
+        todo.setStatus(requestDTO.getStatus());
+
+        return todo;
+    }
+
+    // for entity->DTO mapping
+
+    public static ResponseDTO entityToDToMapping(Todo todo) {
+        ResponseDTO responseDto = new ResponseDTO();
+
+        responseDto.setId(todo.getId());
+        responseDto.setTitle(todo.getTitle());
+        responseDto.setDescription(todo.getDescription());
+        responseDto.setStatus(todo.getStatus());
+        responseDto.setCreatedAt(todo.getCreatedAt());
+
+        return responseDto;
+    }
+}
