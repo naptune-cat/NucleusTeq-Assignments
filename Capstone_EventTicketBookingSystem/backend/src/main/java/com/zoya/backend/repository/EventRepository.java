3efe802d39b711007b025523626d84015a3c9825
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-04-23 15:41:52 +0530

    feat: add repository for event

---
+++ Capstone_EventTicketBookingSystem/backend/src/main/java/com/zoya/backend/repository/EventRepository.java
@@ -0,0 +1,28 @@
+package com.zoya.backend.repository;
+
+public package com.zoya.backend.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.zoya.backend.entity.Event;
+import com.zoya.backend.entity.User;
+import com.zoya.backend.enums.EventStatus;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+    
+    // for all the events by organizer
+    List<Event> findByOrganizer(User organizer);
+    
+    // for Active and future events 
+    List<Event> findByStatusAndEventDateTimeAfter(
+        EventStatus status, LocalDateTime dateTime
+    );
+    
+    // Organizer event by status
+    List<Event> findByOrganizerAndStatus(User organizer, EventStatus status);
+} 
