2966486695cc69da2dbce26b4afdd9d7cee9e479
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-06-10 21:35:04 +0530

    feat: Add custom_math module and add module.py

---
+++ ZoyaKhanam_python_training/Assignment_1/python_basic/custom_math.py
@@ -0,0 +1,19 @@
+"""
+Custom module created for Question 24.
+"""
+
+
+def cube(number):
+    """
+    Return the cube of a number.
+    """
+
+    return number ** 3
+
+
+def square(number):
+    """
+    Return the square of a number.
+    """
+
+    return number ** 2
