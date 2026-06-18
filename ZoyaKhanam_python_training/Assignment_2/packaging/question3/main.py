949000376b78dd250c99ffcf2e06d9f16b9400d7
Zoya Khanam <mkhanzoya8866@gmail.com>
2026-06-18 21:08:46 +0530

    fix: Update import statements to reflect correct module paths

--- ZoyaKhanam_python_training/Assignment_2/packaging/question3/main.py
+++ ZoyaKhanam_python_training/Assignment_2/packaging/question3/main.py
@@ -2,8 +2,8 @@
 Using package modules.
 """
 
-from question3.module1 import show_message as message1
-from question3.module2 import show_message as message2
+from module1 import show_message as message1
+from module2 import show_message as message2
 
 
 def main() -> None:
